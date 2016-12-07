9b5e8ce5e6d306b08a9b7af5b5bbf02e916affd9
lipi <andras.lipoth@gmail.com>
2016-12-07 21:52:42 +1300

    Refactored explosion callbacks

--- rocket.py
+++ rocket.py
@@ -11,13 +11,8 @@ ROCKET_PNG = "pictures/rocket_01_40x69.png"
 ROCKET_HIT_WHITE_PNG = "pictures/rocket_01_40x69_white_01.png"
 ROCKET_HIT_RED_PNG = "pictures/rocket_01_40x69_red_01.png"
 
-EXPLOSION_1 = "pictures/explosion_process/explosion_1.png"
-EXPLOSION_2 = "pictures/explosion_process/explosion_2.png"
-EXPLOSION_3 = "pictures/explosion_process/explosion_3.png"
-EXPLOSION_4 = "pictures/explosion_process/explosion_4.png"
-EXPLOSION_5 = "pictures/explosion_process/explosion_5.png"
-EXPLOSION_6 = "pictures/explosion_process/explosion_6.png"
-EXPLOSION_7 = "pictures/explosion_process/explosion_7.png"
+template = "pictures/explosion_process/explosion_%d.png"
+explosions = [template % i for i in range(1, 8)]
 
 
 class Rocket(Sprite):
@@ -30,6 +25,7 @@ class Rocket(Sprite):
         self.existing_collision = False
         self.explosion_in_progress = False
         self.collision_complete = False
+        self.explosion_step = 0
 
     def update(self):
         if self.first_collision:
@@ -38,27 +34,20 @@ class Rocket(Sprite):
             self.__explode()
             print "################ EXPLODE #######################"
 
-        if not self.collision_complete:
-            return True
-        else:
-            return False
+        return not self.collision_complete
 
     def __explode(self):
         """
         Rocket hits meteorite and explodes
         :return:
         """
-        self.__partial_explode()
+        Clock.schedule_interval(self.__explosion_callback, 0.1)
 
-    def __partial_explode(self):
-        Clock.schedule_once(self.__set_explosion_1, 0)
-        Clock.schedule_once(self.__set_explosion_2, 0.1)
-        Clock.schedule_once(self.__set_explosion_3, 0.2)
-        Clock.schedule_once(self.__set_explosion_4, 0.3)
-        Clock.schedule_once(self.__set_explosion_5, 0.4)
-        Clock.schedule_once(self.__set_explosion_6, 0.5)
-        Clock.schedule_once(self.__set_explosion_7, 0.6)
-        #Clock.schedule_once(self.__set_default_color, 1)
+    def __explosion_callback(self, dt):
+        self.source = explosions[self.explosion_step]
+        self.explosion_step += 1
+        self.collision_complete = (self.explosion_step == len(explosions))
+        return not self.collision_complete
 
     def __set_default_color(self, dt):
         self.source = ROCKET_PNG
@@ -76,29 +65,3 @@ class Rocket(Sprite):
     def __set_red_color(self, dt):
         self.source = ROCKET_HIT_RED_PNG
         self.size = (40, 69)
-
-    def __set_explosion_1(self, dt):
-        self.source = EXPLOSION_1
-
-    def __set_explosion_2(self, dt):
-        self.source = EXPLOSION_2
-
-    def __set_explosion_3(self, dt):
-        self.source = EXPLOSION_3
-
-    def __set_explosion_4(self, dt):
-        self.source = EXPLOSION_4
-        self.size = (60, 70)
-
-    def __set_explosion_5(self, dt):
-        self.source = EXPLOSION_5
-        self.size = (60, 70)
-
-    def __set_explosion_6(self, dt):
-        self.source = EXPLOSION_6
-        self.size = (60, 70)
-
-    def __set_explosion_7(self, dt):
-        self.source = EXPLOSION_7
-        self.size = (60, 70)
-        self.collision_complete = True
