07d1b233ffc231c7c95c1491ea8a40896669d024
mbrouk <michalbroucek@seznam.cz>
2017-09-03 13:10:09 +0200

    Add more unittests for persistence level

--- src/level_persistance.py
+++ src/level_persistance.py
@@ -1,10 +1,14 @@
 __author__ = 'brouk'
 
+import numbers
+import os
+
 from kivy.storage.jsonstore import JsonStore
 
 
 # Module to save and read complete modules - to be persistence between game run
 # - Finished level is read when game starts and new level is saved into the file each time when new level is reach
+
 
 class Persistence:
     """
@@ -13,7 +17,10 @@ class Persistence:
 
     def __init__(self):
         self.JSON_FILE_NAME = 'galaxyrun_level.json'
-        self.store = JsonStore(self.JSON_FILE_NAME)
+        try:
+            self.store = JsonStore(self.JSON_FILE_NAME)
+        except:
+            print "Error cannot create/open persistence store!"
 
     def read_level(self):
         """
@@ -26,7 +33,7 @@ class Persistence:
             if self.store.exists('galaxy_run'):
                 current_level_str = self.store.get('galaxy_run')['level']
                 current_level = int(current_level_str)
-                # print 'Level: ', current_level
+                #print 'Persistence Level read: ', current_level
         except:
             print 'Exception when reading Galaxy run level from JSON file!'
 
@@ -37,5 +44,10 @@ class Persistence:
         Store current level into JSON file
         :return:
         """
-        current_level = str(current_level)
-        self.store.put('galaxy_run', level=current_level, org='brouk')
+        try:
+            if isinstance(current_level, numbers.Number):
+                if 1 <= current_level <= 3:
+                    current_level = str(current_level)
+                    self.store.put('galaxy_run', level=current_level, org='brouk')
+        except:
+            print "Error: cannot save game level!"
--- test/test_level_persistance.py
+++ test/test_level_persistance.py
@@ -1,19 +1,19 @@
 __author__ = 'brouk'
 
 import unittest
-from mock import Mock
-from src import rocket
+import os
 from src import level_persistance
+from kivy.storage.jsonstore import JsonStore
 
 
 class TestLevelPersistence(unittest.TestCase):
-    # preparing to test
+    # preparing to test ... run before each test method
     def setUp(self):
         """ Setting up for the test - setup default Level = 1"""
         persis = level_persistance.Persistence()
         persis.save_level(1)
 
-    # ending the test
+    # ending the test ... run after each test method
     def tearDown(self):
         """Cleaning up after the test - setup default Level = 1"""
         persis = level_persistance.Persistence()
@@ -22,7 +22,6 @@ class TestLevelPersistence(unittest.TestCase):
     def test_read_level_default_valid(self):
         persis = level_persistance.Persistence()
         default_level = persis.read_level()
-        print "Default level : ", default_level
         self.assertEqual(default_level, 1, msg='Default level isn\'t 1 !')
 
     def test_read_level_valid(self):
@@ -32,12 +31,43 @@ class TestLevelPersistence(unittest.TestCase):
         self.assertEqual(level_two, 2, msg='Expected read level isn\'t 2 !')
 
     def test_read_level_no_file(self):
-        pass
+        persis_file_del = level_persistance.Persistence()
+        self.__delete_persistence_file(persis_file_del)
+        persis = level_persistance.Persistence()
+        self.assertEqual(persis.read_level(), 1, msg='Expected read level isn\'t 1. When persistent file is missing !')
 
-    def test_read_level_wrong_file(self):
-        pass
+    # Something wrong here ! - when corrupt file cannot instantiate new Json object ?
+
+    # def test_read_level_corrupted_file(self):
+    #     persis_file_corr = level_persistance.Persistence()
+    #     file_name = persis_file_corr.JSON_FILE_NAME
+    #     persis_file_corr = None
+    #     self.__corrupt__persistence_file(file_name, "Error")
+    #     persis = level_persistance.Persistence()
+    #     self.assertEqual(persis.read_level(), 1, msg='Expected read level isn\'t 1. When persistent file is corrupted !')
 
     def test_write_level_valid(self):
         persis = level_persistance.Persistence()
-        persis.save_level(1)
+        persis.save_level(3)
 
+    def __delete_persistence_file(self, persistence_store):
+        """
+        Helper method to simulate that file was deleted
+        :return:
+        """
+        assert isinstance(persistence_store, level_persistance.Persistence)
+        try:
+            os.remove(persistence_store.JSON_FILE_NAME)
+        except OSError:
+            self.assertTrue(False, msg="Persistence '{0}' file wasn't deleted!".format(persistence_store.JSON_FILE_NAME))
+        except:
+            print "UNKNOWN EXCEPTION WHEN DELETING PERSISTENCE FILE"
+
+    def __corrupt__persistence_file(self, file_name, corrupt_value=""):
+        """
+        Helper method to simulate that persistence file was corrupted
+        :return:
+        """
+        # write corrupted value into the persistence file
+        with open(file_name, "w") as persistence_file:
+            persistence_file.write(corrupt_value)
